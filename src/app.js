11c79d2364aaa93d9dffb4832176993a33a9e32c
Meera Mendhe <meerasmendhe@gmail.com>
2025-09-15 12:20:01 +1000

    Multiple Route handler with diff cases

--- src/app.js
+++ src/app.js
@@ -4,26 +4,35 @@ const app=express()
 
 
 
-// app.get("/user",(req,res)=>{
-//     res.send("Data get successfully");
-// })
-app.post("/user",(req,res)=>{
-    res.send("Post request")
-})
-app.get("/user",(req,res)=>{
-    let query=req.query
-    query=JSON.stringify(query)
-    res.send(`Querys of this url are ---- ${query}`)
-})
+app.get("/user",(req,res,next)=>{
+    console.log("1st handler")
+    // res.send("1st response");
+    next()
+},(req,res)=>{
+    console.log("2nd handler")
+    res.send("2nd response");
 
-app.get("/users/:userID/:name",(req,res)=>{
-    let params=req.params
-    params=JSON.stringify(params)
-    res.send(`Params of this url are ----${params}`)
 })
-app.use((req,res)=>{
-    res.send("Listen to 3000 on server")
+app.get("/test",[(req,res,next)=>{
+    console.log("1st handler")
+    // res.send("1st response");
+    next()
+},(req,res,next)=>{
+    console.log("2nd handler")
+    // res.send("2nd response");
+    next()
+},(req,res,next)=>{
+    console.log("3rd handler")
+    // res.send("3rd response");
+    next()
+}],(req,res,next)=>{
+    console.log("4th handler")
+    res.send("4th response");
+    next()
 })
+// app.use((req,res)=>{
+//     res.send("Listen to 3000 on server")
+// })
 app.listen(3000,()=>{
     console.log("Server is started on port 3000")
 });
